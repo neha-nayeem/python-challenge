41dee0bc314fe461a6b42b7b434e382a82cf3c71
neha-nayeem <61065609+neha-nayeem@users.noreply.github.com>
2020-05-31 07:22:44 -0400

    Update main.py

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,13 +1,13 @@
 # --- import packages to read/write CSV files, create a dynamic path to the file and mean calculation ---
 import csv
 import os
-from statistics import mean  #to calculate average
+import statistics as st #to calculate average
 
 # -- define function to calculate average of the list ---
 def Average(list):
 
     # --- round the number to 2 decimal points and return value ---
-    return round(mean(list), 2) 
+    return round(st.mean(list), 2) 
 
 # --- define relative path for the input and output files ---
 inputfile = os.path.join("Resources", "budget_data.csv")
@@ -22,13 +22,18 @@ ProfLossChanges = []
 with open(inputfile, 'r') as budgetdata:
     reader = csv.reader(budgetdata, delimiter=",")
     
-     # --- read header rows into a Headers list ---
+     # --- store header rows into a Headers list ---
     Headers = next(reader)
 
     # --- for loop to go through each row in the CSV file and append values from date column to the 'Dates' list and Profits/Losses column to the 'ProfLoss' list ---
     for row in reader:
         Dates.append(row[0])
         ProfLoss.append(int(row[1]))
+
+    # --- for loop to go through each value in Profits/Losses list and calculate total ---
+    totalProfLoss = 0
+    for i in ProfLoss:
+        totalProfLoss = i + totalProfLoss
 
     # --- use list comprehension to create a new list with difference values of each successive row (next row - current row) ---
     ProfLossChanges = [ProfLoss[i+1] - ProfLoss[i] for i in range(0,len(ProfLoss)-1)]
@@ -39,9 +44,17 @@ with open(inputfile, 'r') as budgetdata:
     # --- insert a value of zero at index 0 of the ProfLossChanges list as there is no previous data to subtract for the first month (thus also making the list equal in length to Dates and ProfLoss lists for index finding later) ---
     ProfLossChanges.insert(0,0)
 
-# --- calculate greatest increase and decrease in profits and losses using the max and min functions ---
-GreatestIncrease = max(ProfLossChanges)
-GreatestDecrease = min(ProfLossChanges)
+# --- for loop to calculate greatest increase and decrease in profits and losses ---
+GreatestIncrease = 0
+GreatestDecrease = 0
+
+for i in range(len(ProfLossChanges)-1):
+    if ProfLossChanges[i] < GreatestDecrease:
+        GreatestDecrease = ProfLossChanges[i]
+
+    if ProfLossChanges[i] > GreatestIncrease:
+        GreatestIncrease = ProfLossChanges[i]   
+
 
 # --- find the index for the greatest increase and decrease in profits and losses ---
 GIindex = ProfLossChanges.index(GreatestIncrease)
@@ -55,7 +68,7 @@ GDdate = Dates[GDindex]
 analysisOutput = (f"Financial Analysis\n"
                   f"----------------------------\n"
                   f"Total Months: {len(Dates)}\n"
-                  f"Total: ${sum(ProfLoss)}\n"
+                  f"Total: ${totalProfLoss}\n"
                   f"Average Change: ${AverageChange}\n"
                   f"Greatest Increase in Profits: {GIdate} (${GreatestIncrease})\n"
                   f"Greatest Decrease in Profits: {GDdate} (${GreatestDecrease})\n"
